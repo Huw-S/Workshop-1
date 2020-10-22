44adc88afaacdab6fa406f9a9c49bfe3f7182a96
DexterWheel <dw1182@york.ac.uk>
2020-10-22 17:10:18 +0100

    Finnished

--- scripts/data_analysis.R
+++ scripts/data_analysis.R
@@ -8,5 +8,20 @@ mod <- lm(data = chaff2, mass ~ sex)
 mod
 summary(mod)
 
-ggplot(data)
+fig1 <- ggplot(data = chaff2, aes(x = sex, y = mass, fill = sex))+
+  geom_boxplot()+
+  theme_classic()
 
+units <- "in"  
+fig_w <- 3.5
+fig_h <- fig_w
+dpi <- 300
+device <- "tiff" 
+
+ggsave("figures/fig1.tiff",
+       plot = fig1,
+       device = device,
+       width = fig_w,
+       height = fig_h,
+       units = units,
+       dpi = dpi)

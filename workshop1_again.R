50e97b1ac9afec51507c7af2cd09f1584ab0fb84
Huw Stephens <hjrs500@york.ac.uk>
2020-10-22 17:05:52 +0100

    c

--- workshop1_again.R
+++ workshop1_again.R
@@ -5,3 +5,11 @@ library(tidyr)
 chaff2 <- chaff%>%
   pivot_longer(names_to = "sex", values_to = "mass",
                cols = everything())
+
+mod <- lm(data = chaff2, mass ~ sex)
+mod=
+summary(mod)
+
+ggplot(data = chaff2, aes(x = sex, y = mass, fill = sex))+
+  geom_boxplot()+
+  theme_classic()

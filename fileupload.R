965c9c014430138ca524f3b0af2dfa4d2cdfdd1b
DexterWheel <dw1182@york.ac.uk>
2020-10-22 16:12:34 +0100

    egg

--- fileupload.R
+++ fileupload.R
@@ -1,13 +1,18 @@
-file <-  "../data/chaff.txt"
-write.table(chaff2, 
-            file, 
-            quote = FALSE,
-            row.names = FALSE)
+chaff <- read.table("data/chaff.txt", header=TRUE)
 
-
-
+#tidying data
+library(tidyverse)
 
 chaff2<- chaff%>% 
   pivot_longer(names_to = "sex", 
                values_to = "mass",
                cols = everything())
+
+#defining name of file 
+file <-  "data/chaff2.txt"
+
+#saving tidied file
+write.table(chaff2, 
+            file, 
+            quote = FALSE,
+            row.names = FALSE)
